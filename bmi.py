e7e950d4dac40ba83b6bf73a267cb02878146968
Keane <keanegsm9805@gmail.com>
2024-11-06 22:33:16 +0800

    This is with return values

--- bmi.py
+++ bmi.py
@@ -10,10 +10,10 @@ def calculate_bmi(height, weight):
         return -1
     elif bmi >25.0:
         print("You are overweight")
-        return 0 
+        return 1 
     else: 
         print("You are Normal Weight")
-        return 1
+        return 0
 
 calculate_bmi(weight = 57, height = 1.73)
 print("==========")
