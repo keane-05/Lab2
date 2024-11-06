46160d80b4844e9488569ac273b4c8ab1a7c3810
Keane <keanegsm9805@gmail.com>
2024-11-06 11:31:50 +0800

    added return values

--- bmi.py
+++ bmi.py
@@ -7,13 +7,16 @@ def calculate_bmi(height, weight):
     # use round(variable, dp), this is so that you can round to the nearest dp
     if bmi < 18.5: 
         print("You are underweight")
+        return -1
     elif bmi >25.0:
         print("You are overweight")
+        return 0 
     else: 
         print("You are Normal Weight")
+        return 1
 
 calculate_bmi(weight = 57, height = 1.73)
 print("==========")
-calculate_bmi(weight = 57, height = 1.73)
+calculate_bmi(weight = 20, height = 1.73)
 print("==========")
-calculate_bmi(weight = 57, height = 1.73)
+calculate_bmi(weight = 100, height = 1.73)
